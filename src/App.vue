2abbeff4b50d0d697a0499ba30510aa31fe69128
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-01-20 20:52:28 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="container mt-5">
+    <vee-validate-form />
+    <!-- <vuelidate-form /> -->
+  </div>
+</template>
+
+<script>
+import VeeValidateForm from "./components/VeeValidateForm";
+// import VuelidateForm from "./components/VuelidateForm";
+
+export default {
+  name: "App",
+  components: {
+    VeeValidateForm,
+    // VuelidateForm
+  }
+};
+</script>
+
+<style>
+@import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
+</style>
